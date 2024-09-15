c664b5461ff3553aecff9f1fd9127ce20dde2059
ujjawal saini <ujjawalsaini1@gmail.com>
2024-09-15 10:00:00 +0530

    add cors + dotenv, basic middleware

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is Live...");
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
