ca7d6eaccb15f0bdd6f3a52160e41e16caed5e9f
ujjawal saini <ujjawalsaini1@gmail.com>
2024-09-22 10:42:17 +0530

    added user routes connection

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 const app = express();
 
@@ -11,6 +12,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is Live...");
 });
+
+app.use("/api/users", userRouter);
 
 const PORT = process.env.PORT || 3000;
 
