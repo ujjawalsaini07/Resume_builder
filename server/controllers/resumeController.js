cbf6e4ee46398049c443f85dff6f09f8f390e1eb
ujjawal saini <ujjawalsaini1@gmail.com>
2024-09-24 10:38:21 +0530

    add resume controller: create/get/delete

---
+++ server/controllers/resumeController.js
@@ -0,0 +1,54 @@
+import Resume from "../models/Resume.js";
+
+// controller for creating new resume
+// POST : /api/resumes/create
+export const createResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    const { title } = req.body;
+
+    const newResume = await Resume.create({ userId, title });
+    return res.status(201).json({ message: "Resume created successfully", resume: newResume });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// controller for deleting resume
+// DELETE : /api/resumes/delete
+export const deleteResume = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const resumeId = req.params.resumeId;
+
+    const deletedResume = await Resume.findOneAndDelete({ userId, _id: resumeId });
+
+    if (!deletedResume) {
+      return res.status(404).json({ message: "Resume not found" });
+    }
+
+    return res.status(200).json({ message: "Resume deleted successfully" });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// get resume by id
+// GET : /api/resumes/get
+export const getResumeById = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const resumeId = req.params.resumeId;
+
+    const resume = await Resume.findOne({ userId, _id: resumeId });
+
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
+    }
+
+    return res.status(200).json({ resume });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
