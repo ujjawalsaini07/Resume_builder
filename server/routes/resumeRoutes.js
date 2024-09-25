55c0f5989171bc803adc09ab8ef617470969531d
ujjawal saini <ujjawalsaini1@gmail.com>
2024-09-25 11:24:18 +0530

    make public resume route actually public

--- server/routes/resumeRoutes.js
+++ server/routes/resumeRoutes.js
@@ -16,6 +16,6 @@ resumeRouter.delete("/delete/:resumeId", protect, deleteResume);
 
 resumeRouter.get("/get/:resumeId", protect, getResumeById);
 
-resumeRouter.get("/public/:resumeId", protect, getPublicResumeById);
+resumeRouter.get("/public/:resumeId", getPublicResumeById);
 
 export default resumeRouter;
